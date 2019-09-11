6b2f7325ec53da817b19c830be64554b16293b46
Dhruv <dhruvin725@gmail.com>
2019-09-11 00:32:34 -0400

    Create the ground in js file

--- labs/GroundRain/js/app.js
+++ labs/GroundRain/js/app.js
@@ -1,7 +1,7 @@
 
 class Drop {
     constructor(){
-        this.x = 40;
+        this.x = Math.random() * 400;
         this.y = 0;
     }
 
@@ -11,15 +11,54 @@ class Drop {
         circle(this.x, this.y, 5);
     }
 }
-class Rain {
+class RainManager {
     constructor(){
         this.drops = [];
     }
     createDrop(){
-        //stub
-        //TODO: complete
+        //Make a new drop
+        var newDrop = new Drop();
+
+        //add this new drop to our collection of drops
+        this.drops.push(newDrop);
+    }
+
+    update(){
+        for(var i=0; i< this.drops.length; i++){
+            this.drops[i].update();
+        }
+        console.log(this.drops[0]);
     }
 }
+
+class Ground{
+    //constructor
+        //set the starting color
+        //start the drop hit count
+    constructor(){
+        this.color =255;
+        this.dropHit = 0;
+    }
+    //update - draws the rectangle to the screen 
+    update(){
+   
+        fill(this.color);
+        stroke(0);
+        strokeWeight(2);
+        rect(2,250,395,298);
+    }
+    
+    
+
+    //drop hit - called when a rain drop gets low enough (how do you inform it?)
+   
+        //change the color for every ten rain drops hit
+
+}
+
+//global variables
+var rainManager = new RainManager();
+var ground = new Ground();
 
 //run ones before the app starts
 function setup(){
@@ -27,5 +66,15 @@ function setup(){
 }
 // run 60 times a second, or so
 function draw(){
-    d.update();
+    // clear out background
+    background(255);
+
+    // create a new drop on a 5% chance
+    if (Math.random() < 0.05){
+        rainManager.createDrop();
+
+    }
+
+    rainManager.update();
+    ground.update();
 }
