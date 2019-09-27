f9d9044426fad88abfaf3e6353322ab3202fcdc2
Dhruv <dhruvin725@gmail.com>
2019-09-26 23:48:54 -0400

    Few changes on final assignment

--- labs/AnimationLab/js/app.js
+++ labs/AnimationLab/js/app.js
@@ -1,13 +1,26 @@
-var Box1 = document.querySelector("#box1");
-var Box2 = document.querySelector("#box2");
 
- Box1.addEventListener("mouseover", onCardOver);
- Box1.addEventListener("mouseout", onCardOut);
+var Place = document.querySelector("#place");
+var BoxTemplate = document.querySelector("#boxTemplate");
+
+ for(let i=0; i<6; i++){
+ let newCard = document.createElement("div");
+  
+    newCard.classList.add("boxes");
+    newCard.style.animationDelay = i*0.3 + "s";
+    
+    //set up connections and events
+    newCard.addEventListener("mouseover", onCardOver);
+    newCard.addEventListener("mouseout", onCardOut);
+    
+    //put onto the page
+    Place.appendChild(newCard);
+ }
 
 function onCardOver(event) {
-  event.target.style.animationDelay = "0s";
+  
   event.target.classList.add("cardOver");
   event.target.classList.remove("cardOut");
+  event.target.style.animationDelay = "0s";
 }
 
 function onCardOut(event) {
