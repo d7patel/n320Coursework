86d4c5829188ed578703aaf36d3538a62f11bc99
Dhruv <dhruvin725@gmail.com>
2019-10-29 23:44:38 -0400

    submitting assignment

---
+++ labs/DSandDisplay/js/app.js
@@ -0,0 +1,16 @@
+
+var app = new Vue({
+    el: "#app",
+    mounted: function() {
+        axios.get('data/candy.json')
+        .then (( response ) => {
+            this.candy = response.data.candy;
+        })
+    },
+    data: {
+        candy: [ ]
+    },
+    methods: {
+
+    }
+})
