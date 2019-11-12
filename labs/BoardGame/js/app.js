d368fa85d8aad4052aca156dce866c39fbe3b6d7
Dhruv <dhruvin725@gmail.com>
2019-11-12 02:14:48 -0500

    Few changes

--- labs/BoardGame/js/app.js
+++ labs/BoardGame/js/app.js
@@ -3,7 +3,7 @@ new Vue ({
   data: {
   	playerHealth: 100,
   	monsterHealth: 100,
-  	playGame: true,
+    playGame: true,
    
     player: '',
     monster: '',
@@ -11,13 +11,15 @@ new Vue ({
   },
   methods :{
   
+    //Set 100 health for the both player in thr beginning of the game.
     startGame : function (){
       this.playGame = true;
       this.damage = 0;
   		this.playerHealth = 100;
   		this.monsterHealth = 100;
-      
     },
+
+    //Simple attack that will reduce more life of user then monster
     fight : function(){
       var damage = this.claculateDamage(5, 10);
       this.monsterHealth -= damage;
@@ -30,6 +32,7 @@ new Vue ({
       this.damage = totalDamage;
     },
     
+    //Special attack to reduce more health of monster
     powerAttack : function(){
       var damage = this.claculateDamage(20, 40);
       this.monsterHealth -= damage;
@@ -42,6 +45,7 @@ new Vue ({
       this.damage = totalDamage;
     },
     
+    //Give option to increase the health for user
     powerUp : function() {
       if (this.playerHealth <= 80) {
         this.playerHealth += 20;  
@@ -55,12 +59,17 @@ new Vue ({
       this.damage = totalDamage;
     },
     
+    //Give option togive up the game for user
     giveUp : function(){
-      this.playGame = true;
+      this.playGame = false;
   	  this.playerHealth = 0;	
       this.damage = 0;
+      if (this.checkWin()) {
+        return;
+      }
     },
     
+    //This function will reduce user health whenever he attack to the monster
     monsterAttacks: function() {
       var damage = this.claculateDamage(10, 15);
       this.playerHealth -= damage;
@@ -68,13 +77,16 @@ new Vue ({
       this.monster = damage;
     },
     
+    //Generated random attack.
     claculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
 
+    //Create function to check who win (user or monster) 
+    //Ask user if they wanna play again.
     checkWin: function() {
       if (this.monsterHealth <= 0) {
-        if (confirm('You won!!!   start a new Game???')) {
+        if (confirm('You WON. Do you wanna start a new Game?')) {
           this.startGame();
         }
         else{
@@ -85,7 +97,7 @@ new Vue ({
         return true;
       } 
       else if (this.playerHealth <= 0) {
-        if (confirm('You lost!!!   start a new Game???')) {
+        if (confirm('You lost. Do you wanna start a new Game?')) {
           this.startGame();
         } 
         else{
@@ -97,6 +109,5 @@ new Vue ({
       }
       return false;
     }
-
   }
 });
