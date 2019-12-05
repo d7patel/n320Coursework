ac910dc9d437e4b9abfdd48af46a101e9aa073fb
Dhruv <dhruvin725@gmail.com>
2019-12-05 16:16:54 -0500

    Uploading kickball lab

--- labs/3DBrain/js/app.js
+++ labs/3DBrain/js/app.js
@@ -12,6 +12,13 @@ function createScene() {
   // Create the scene space
   var scene = new BABYLON.Scene(engine);
 
+  var radius = 3;
+  var deltaTheta = 0;
+  var path = [];
+for(var theta = 0; theta < 2 * Math.PI; theta +=deltaTheta ) {
+    path.push(new BABYLON.Vector3(radius * Math.cos(theta), radius * Math.sin(theta), 0)); }
+
+
   // Add a camera to the scene and attach it to the canvas
   camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 4, BABYLON.Vector3.Zero(), scene);
   camera.attachControl(canvas, true);
@@ -20,22 +27,9 @@ function createScene() {
   var myLight = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, 1.0), scene);
 
   // Add and manipulate meshes in the scene
-  sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: .5}, scene);
-  var lesserSphere = BABYLON.MeshBuilder.CreateSphere("sphere2", {diameter: .5}, scene);
-  lesserSphere.position.x = 0.9;
-
-  var lesser2Sphere = BABYLON.MeshBuilder.CreateSphere("sphere3", {diameter: .5}, scene);
-  lesser2Sphere.position.x = 1.8;
-  
-  sphere.addChild(lesserSphere);
-  light = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(1, 1, 0), scene);
-
-    blueMat = new BABYLON.StandardMaterial("ground", scene);
-    blueMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
-    blueMat.specularColor = new BABYLON.Color3(0.4, 0.4, 0.4);
-    blueMat.emissiveColor = BABYLON.Color3.Blue();
-
+  //sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: .5}, scene);
     
+  var curve = BABYLON.Curve3.Create.CURVETYPE(parameters);
   return scene;
 };
 
@@ -54,20 +48,12 @@ function checkUp() {
 
 window.addEventListener("keydown", (event) => {
 
-    if(selectedMesh) {
-        if(event.keyCode == 87) {
-            TweenLite.to(selectedMesh.rotation, 1, { x: "+=20", onComplete: checkUp });
-        }
-    }
+
 
 })
 
 window.addEventListener("click", function () {
     // We try to pick an object
-    var pickResult = scene.pick(scene.pointerX, scene.pointerY);
-
-    pickResult.pickedMesh.material = blueMat;
-
-    selectedMesh = pickResult.pickedMesh;
+   
 })
 
